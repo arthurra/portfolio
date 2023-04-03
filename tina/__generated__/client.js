22d28982048d7eabe2d0015f674b1e5551c94113
Ryan Arthur <ryan@ryanarthurdigital.com>
2023-04-03 00:33:33 -0700

    Fix syntax. Add package.json

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'null', queries });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '109a83f40fadcb4e93b1a503679349fd7c113078', queries });
 export default client;
   
